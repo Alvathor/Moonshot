be82a8b85d212112a111963a85fbe89c495106f8
Juliano <alvarenga.vix@gmail.com>
2020-01-01 10:58:29 -0300

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Juliano Goncalves Alvarenga on 01/01/20.
+//  Copyright © 2020 Ciclic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
